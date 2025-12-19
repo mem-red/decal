dcc5edf2425c3b9bbb850241164bf21f7bbf3a16
zignis <77036902+zignis@users.noreply.github.com>
2025-12-19 21:07:18 +0530

    remove dbg assertion

--- decal-core/src/capabilities/opacity.rs
+++ decal-core/src/capabilities/opacity.rs
@@ -1,9 +1,11 @@
 use super::Drawable;
 
 pub trait Opacity: Drawable {
-    fn opacity(mut self, value: f32) -> Self {
-        debug_assert!(value >= 0.0 && value <= 1.0);
-        self.visual_mut().opacity = value.clamp(0.0, 1.0);
+    fn opacity<T>(mut self, value: T) -> Self
+    where
+        T: Into<f32>,
+    {
+        self.visual_mut().opacity = value.into().clamp(0.0, 1.0);
         self
     }
 }
