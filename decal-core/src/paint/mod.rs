734fe47e3d30e9941f3790092b949f277920f3a3
zignis <77036902+zignis@users.noreply.github.com>
2025-12-19 08:12:25 +0530

    add resources

--- decal-core/src/paint/mod.rs
+++ decal-core/src/paint/mod.rs
@@ -1,7 +1,9 @@
 mod appearance;
+mod resources;
 mod round_rect;
 mod scaled_radii;
 
 pub(crate) use appearance::*;
+pub(crate) use resources::*;
 pub(crate) use round_rect::*;
 pub(crate) use scaled_radii::*;
