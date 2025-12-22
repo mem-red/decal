f28cd9ac5eea9e858296c2ace79512f6181ddb46
zignis <77036902+zignis@users.noreply.github.com>
2025-12-22 23:26:03 +0530

    progress on gradients

--- decal-core/src/primitives/linear_gradient.rs
+++ decal-core/src/primitives/linear_gradient.rs
@@ -1,15 +1,33 @@
 use crate::paint::ResourceIri;
-use crate::primitives::Stop;
+use crate::prelude::Color;
+use crate::primitives::{GradientTransform, Stop};
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Default)]
 pub struct LinearGradient {
     stops: Vec<Stop>,
+    transform: GradientTransform,
 }
 
 impl LinearGradient {
     pub fn new() -> Self {
-        LinearGradient { stops: Vec::new() }
+        LinearGradient {
+            stops: vec![
+                Stop::new()
+                    .offset(0.1)
+                    .color(Color::parse("red"))
+                    .opacity(0.5),
+                Stop::new()
+                    .offset(0.4)
+                    .color(Color::parse("blue"))
+                    .opacity(0.5),
+                Stop::new()
+                    .offset(0.9)
+                    .color(Color::parse("yellow"))
+                    .opacity(0.5),
+            ],
+            ..Default::default()
+        }
     }
 }
 
@@ -17,11 +35,14 @@ impl ResourceIri for LinearGradient {}
 
 impl Display for LinearGradient {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, r#"<linearGradient id={}"#, self.iri())?;
+        write!(f, r#"<linearGradient id="{}""#, self.iri())?;
+        self.transform.write(f)?;
 
         if self.stops.is_empty() {
-            write!(f, ">")
+            write!(f, " />")
         } else {
+            write!(f, ">")?;
+
             for stop in &self.stops {
                 write!(f, "{stop}")?;
             }
--- decal-core/src/primitives/radial_gradient.rs
+++ decal-core/src/primitives/radial_gradient.rs
@@ -1,15 +1,17 @@
 use crate::paint::ResourceIri;
+use crate::prelude::GradientTransform;
 use crate::primitives::Stop;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Default)]
 pub struct RadialGradient {
     stops: Vec<Stop>,
+    transform: GradientTransform,
 }
 
 impl RadialGradient {
     pub fn new() -> Self {
-        RadialGradient { stops: Vec::new() }
+        RadialGradient::default()
     }
 }
 
@@ -17,11 +19,14 @@ impl ResourceIri for RadialGradient {}
 
 impl Display for RadialGradient {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, r#"<radialGradient id={}"#, self.iri())?;
+        write!(f, r#"<radialGradient id="{}""#, self.iri())?;
+        self.transform.write(f)?;
 
         if self.stops.is_empty() {
-            write!(f, ">")
+            write!(f, " />")
         } else {
+            write!(f, ">")?;
+
             for stop in &self.stops {
                 write!(f, "{stop}")?;
             }
