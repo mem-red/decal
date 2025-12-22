acff3acf21659df72e5d2bb9690ce1674100c258
zignis <77036902+zignis@users.noreply.github.com>
2025-12-22 16:59:44 +0530

    update `linear_gradient`

--- decal-core/src/primitives/linear_gradient.rs
+++ decal-core/src/primitives/linear_gradient.rs
@@ -1,19 +1,14 @@
-use crate::primitives::ResourceIri;
+use crate::primitives::{ResourceIri, Stop};
 use std::fmt::{Display, Formatter};
-use strict_num::PositiveF32;
 
-#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone, Default)]
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Default)]
 pub struct LinearGradient {
-    start: PositiveF32,
-    end: PositiveF32,
+    stops: Vec<Stop>,
 }
 
 impl LinearGradient {
-    pub fn new(start: f32, end: f32) -> Self {
-        LinearGradient {
-            start: PositiveF32::new(start).unwrap_or_default(),
-            end: PositiveF32::new(end).unwrap_or_default(),
-        }
+    pub fn new() -> Self {
+        LinearGradient { stops: Vec::new() }
     }
 }
 
@@ -21,15 +16,16 @@ impl ResourceIri for LinearGradient {}
 
 impl Display for LinearGradient {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#"<linearGradient id="{}">
-                <stop offset="{}%" stop-color="blue" />
-                <stop offset="{}%" stop-color="red" />
-            </linearGradient>"#,
-            self.iri(),
-            self.start,
-            self.end
-        )
+        write!(f, r#"<linearGradient id={}"#, self.iri())?;
+
+        if self.stops.is_empty() {
+            write!(f, ">")
+        } else {
+            for stop in &self.stops {
+                write!(f, "{stop}")?;
+            }
+
+            write!(f, r#"</linearGradient>"#)
+        }
     }
 }
