8ea0f49c17a295eeda98bad61d964146aa538d02
zignis <77036902+zignis@users.noreply.github.com>
2025-12-22 17:11:28 +0530

    improve `Display` for `radial_gradient`

--- decal-core/src/primitives/radial_gradient.rs
+++ decal-core/src/primitives/radial_gradient.rs
@@ -1,20 +1,32 @@
-use crate::primitives::ResourceIri;
+use crate::paint::ResourceIri;
+use crate::primitives::Stop;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
-pub struct RadialGradient {}
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
+pub struct RadialGradient {
+    stops: Vec<Stop>,
+}
+
+impl RadialGradient {
+    pub fn new() -> Self {
+        RadialGradient { stops: Vec::new() }
+    }
+}
 
 impl ResourceIri for RadialGradient {}
 
 impl Display for RadialGradient {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#"<radialGradient id="{}">
-                <stop offset="5%" stop-color="blue" />
-                <stop offset="80%" stop-color="red" />
-            </radialGradient>"#,
-            self.iri(),
-        )
+        write!(f, r#"<radialGradient id={}"#, self.iri())?;
+
+        if self.stops.is_empty() {
+            write!(f, ">")
+        } else {
+            for stop in &self.stops {
+                write!(f, "{stop}")?;
+            }
+
+            write!(f, r#"</radialGradient>"#)
+        }
     }
 }
