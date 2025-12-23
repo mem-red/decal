2deff693131c1dfa527569054fa81250175fa3f3
zignis <77036902+zignis@users.noreply.github.com>
2025-12-23 12:03:50 +0530

    add units to `linear_gradient`

--- decal-core/src/primitives/linear_gradient.rs
+++ decal-core/src/primitives/linear_gradient.rs
@@ -1,15 +1,38 @@
 use crate::paint::ResourceIri;
 use crate::prelude::Color;
-use crate::primitives::{GradientTransform, Stop};
+use crate::primitives::{GradientTransform, Length, Stop};
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Hash, Eq, PartialEq, Clone, Default)]
+type GradientUnit = Length<false, true>;
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone)]
 pub struct LinearGradient {
     stops: Vec<Stop>,
+    x1: GradientUnit,
+    y1: GradientUnit,
+    x2: GradientUnit,
+    y2: GradientUnit,
     transform: GradientTransform,
 }
 
+impl Default for LinearGradient {
+    fn default() -> Self {
+        LinearGradient {
+            stops: Vec::new(),
+            x1: GradientUnit::zero(),
+            y1: GradientUnit::zero(),
+            x2: GradientUnit::percent(100),
+            y2: GradientUnit::zero(),
+            transform: GradientTransform::default(),
+        }
+    }
+}
+
 impl LinearGradient {
+    // pub fn new() -> Self {
+    //     LinearGradient::default()
+    // }
+
     pub fn new() -> Self {
         LinearGradient {
             stops: vec![
@@ -28,6 +51,47 @@ impl LinearGradient {
             ],
             ..Default::default()
         }
+    }
+
+    pub fn x1<T>(mut self, value: T) -> Self
+    where
+        T: Into<Option<GradientUnit>>,
+    {
+        self.x1 = value.into().unwrap_or_default();
+        self
+    }
+
+    pub fn y1<T>(mut self, value: T) -> Self
+    where
+        T: Into<Option<GradientUnit>>,
+    {
+        self.y1 = value.into().unwrap_or_default();
+        self
+    }
+
+    pub fn x2<T>(mut self, value: T) -> Self
+    where
+        T: Into<Option<GradientUnit>>,
+    {
+        self.x2 = value.into().unwrap_or(GradientUnit::percent(100));
+        self
+    }
+
+    pub fn y2<T>(mut self, value: T) -> Self
+    where
+        T: Into<Option<GradientUnit>>,
+    {
+        self.y2 = value.into().unwrap_or_default();
+        self
+    }
+
+    // TODO: use trait here
+    pub fn transform<T>(mut self, value: T) -> Self
+    where
+        T: Into<Option<GradientTransform>>,
+    {
+        self.transform = value.into().unwrap_or_default();
+        self
     }
 }
 
