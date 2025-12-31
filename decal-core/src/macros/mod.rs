1b2e8ca4463ecd24e28fdbbc4168bd91b3a9619d
zignis <77036902+zignis@users.noreply.github.com>
2025-12-31 08:41:25 +0530

    impl float macros

--- decal-core/src/macros/mod.rs
+++ decal-core/src/macros/mod.rs
@@ -1,8 +1,12 @@
 mod finite_f32;
 mod node_builder;
+mod normalized_f32;
+mod positive_f32;
 
 pub(crate) use finite_f32::*;
 pub(crate) use node_builder::*;
+pub(crate) use normalized_f32::*;
+pub(crate) use positive_f32::*;
 
 // Public macros re-export
 pub use decal_macros::*;
