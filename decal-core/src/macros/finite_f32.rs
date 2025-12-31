f8ff01e5175efdd265d7d351ccda4a9edcc3b30b
zignis <77036902+zignis@users.noreply.github.com>
2025-12-31 08:31:55 +0530

    improve `ff32!`

--- decal-core/src/macros/finite_f32.rs
+++ decal-core/src/macros/finite_f32.rs
@@ -1,10 +1,11 @@
 // finite f32
 macro_rules! ff32 {
     ($expr:expr) => {
-        FiniteF32::new($expr).unwrap_or_default()
+        strict_num::FiniteF32::new($expr.into()).unwrap_or_default()
     };
     ($expr:expr, $default:expr) => {
-        FiniteF32::new($expr).unwrap_or_else(|| FiniteF32::new($default).unwrap_or_default())
+        strict_num::FiniteF32::new($expr.into())
+            .unwrap_or_else(|| strict_num::FiniteF32::new($default.into()).unwrap_or_default())
     };
 }
 
