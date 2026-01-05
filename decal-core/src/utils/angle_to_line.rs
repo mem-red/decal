10d0ca8aec9a8af92bfa5543cbe80134e3955b59
zignis <77036902+zignis@users.noreply.github.com>
2026-01-05 19:31:10 +0530

    update angle to line compute logic

--- decal-core/src/utils/angle_to_line.rs
+++ decal-core/src/utils/angle_to_line.rs
@@ -3,10 +3,17 @@ use std::f32::consts::PI;
 const CX: f32 = 0.5;
 const CY: f32 = 0.5;
 
+// https://observablehq.com/@danburzo/css-gradient-line
+
 pub(crate) fn angle_to_line(angle: f32) -> (f32, f32, f32, f32) {
     let rad = (90.0 - angle) * PI / 180.0;
-    let dx = rad.cos() / 2.0;
-    let dy = rad.sin() / 2.0;
+    let cos = rad.cos();
+    let sin = rad.sin();
+    let scale = 0.5 / cos.abs().max(sin.abs()); // cover the box
+
+    let dx = cos * scale;
+    let dy = sin * scale;
+
     let x1 = CX - dx;
     let y1 = CY + dy; // y-axis is down
     let x2 = CX + dx;
