a5fe62fa40a670ead77a0dd773cc5e0a04cb5c0b
zignis <77036902+zignis@users.noreply.github.com>
2026-01-06 11:14:16 +0530

    update `round_rect`

--- decal-core/src/paint/round_rect.rs
+++ decal-core/src/paint/round_rect.rs
@@ -1,4 +1,5 @@
 use crate::paint::ScaledRadii;
+use crate::primitives::Rect;
 use crate::utils::PathWriter;
 
 pub(crate) fn write_fill_path<T>(out: &mut T, w: f32, h: f32, r: ScaledRadii) -> std::fmt::Result
@@ -13,18 +14,18 @@ pub(crate) fn write_border_path<T>(
     w: f32,
     h: f32,
     r: ScaledRadii,
-    borders: (f32, f32, f32, f32),
+    border: Rect<f32>,
 ) -> std::fmt::Result
 where
     T: std::fmt::Write,
 {
-    let (bt, br, bb, bl) = borders;
+    let (bt, br, bb, bl) = border.tuple();
     if bt + br + bb + bl == 0.0 {
         return Ok(());
     }
 
     write_round_rect(out, 0.0, 0.0, w, h, r)?; // outer ring
-    write_round_rect(out, bl, bt, w - br, h - bb, inset_radii(r, borders))?; // inner ring
+    write_round_rect(out, bl, bt, w - br, h - bb, inset_radii(r, border))?; // inner ring
 
     Ok(())
 }
@@ -34,7 +35,7 @@ pub(crate) fn write_clip_path<T>(
     w: f32,
     h: f32,
     r: ScaledRadii,
-    borders: (f32, f32, f32, f32),
+    border: Rect<f32>,
     clip_x: bool,
     clip_y: bool,
     root_size: (f32, f32),
@@ -42,9 +43,9 @@ pub(crate) fn write_clip_path<T>(
 where
     T: std::fmt::Write,
 {
-    let (bt, br, bb, bl) = borders;
+    let (bt, br, bb, bl) = border.tuple();
     let (root_w, root_h) = root_size;
-    let ir = inset_radii(r, borders);
+    let ir = inset_radii(r, border);
     let (x1, y1) = (bl, bt);
     let (x2, y2) = (w - br, h - bb);
 
@@ -60,9 +61,8 @@ where
     }
 }
 
-fn inset_radii(r: ScaledRadii, borders: (f32, f32, f32, f32)) -> ScaledRadii {
-    let (bt, br, bb, bl) = borders;
-
+fn inset_radii(r: ScaledRadii, border: Rect<f32>) -> ScaledRadii {
+    let (bt, br, bb, bl) = border.tuple();
     ScaledRadii {
         h_tl: (r.h_tl - bl).max(0.0),
         h_tr: (r.h_tr - br).max(0.0),
@@ -89,22 +89,22 @@ where
 {
     if r.h_tl + r.h_tr + r.h_br + r.h_bl == 0.0 {
         PathWriter::new(out)
-            .move_to(x1, y1)
-            .horizontal_to(x2)
-            .vertical_to(y2)
-            .horizontal_to(x1)
+            .move_to(x1, y1)?
+            .horizontal_to(x2)?
+            .vertical_to(y2)?
+            .horizontal_to(x1)?
             .close()
     } else {
         PathWriter::new(out)
-            .move_to(x1 + r.h_tl, y1)
-            .horizontal_to(x2 - r.h_tr)
-            .arc_to(r.h_tr, r.v_tr, x2, y1 + r.v_tr)
-            .vertical_to(y2 - r.v_br)
-            .arc_to(r.h_br, r.v_br, x2 - r.h_br, y2)
-            .horizontal_to(x1 + r.h_bl)
-            .arc_to(r.h_bl, r.v_bl, x1, y2 - r.v_bl)
-            .vertical_to(y1 + r.v_tl)
-            .arc_to(r.h_tl, r.v_tl, x1 + r.h_tl, y1)
+            .move_to(x1 + r.h_tl, y1)?
+            .horizontal_to(x2 - r.h_tr)?
+            .arc_to(r.h_tr, r.v_tr, x2, y1 + r.v_tr)?
+            .vertical_to(y2 - r.v_br)?
+            .arc_to(r.h_br, r.v_br, x2 - r.h_br, y2)?
+            .horizontal_to(x1 + r.h_bl)?
+            .arc_to(r.h_bl, r.v_bl, x1, y2 - r.v_bl)?
+            .vertical_to(y1 + r.v_tl)?
+            .arc_to(r.h_tl, r.v_tl, x1 + r.h_tl, y1)?
             .close()
     }
 }
