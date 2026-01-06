16b4232842f697252b4ac420ad2f00bf9293d10a
zignis <77036902+zignis@users.noreply.github.com>
2026-01-06 11:11:29 +0530

    update `Color`

--- decal-core/src/primitives/color.rs
+++ decal-core/src/primitives/color.rs
@@ -1,5 +1,6 @@
+use crate::utils::FloatWriter;
 use color::{AlphaColor, Srgb, parse_color};
-use std::fmt::Display;
+use std::fmt::{Display, Write};
 
 #[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub struct Color {
@@ -15,13 +16,12 @@ impl Color {
     }
 
     pub const fn rgba(r: u8, g: u8, b: u8, a: f32) -> Self {
-        debug_assert!(a >= 0.0 && a <= 100.0);
-
+        debug_assert!(a >= 0.0 && a <= 1.0);
         Self {
             r,
             g,
             b,
-            a: ((a / 100.0).clamp(0.0, 1.0) * 255.0) as u8,
+            a: (a.clamp(0.0, 1.0) * 255.0) as u8,
         }
     }
 
@@ -46,18 +46,15 @@ impl Display for Color {
         if self.a == 255 {
             f.write_fmt(format_args!("rgb({},{},{})", self.r, self.g, self.b))
         } else {
-            f.write_fmt(format_args!(
-                "rgba({},{},{},{:.2})",
-                self.r,
-                self.g,
-                self.b,
-                self.a as f32 / 255.0
-            ))
+            f.write_fmt(format_args!("rgba({},{},{},", self.r, self.g, self.b))?;
+            f.write_float(self.a as f32 / 255.0)?;
+            f.write_char(')')
         }
     }
 }
 
 impl From<color::Rgba8> for Color {
+    #[inline]
     fn from(value: color::Rgba8) -> Self {
         Self {
             r: value.r,
@@ -69,9 +66,39 @@ impl From<color::Rgba8> for Color {
 }
 
 impl From<color::DynamicColor> for Color {
+    #[inline]
     fn from(color: color::DynamicColor) -> Self {
         let srgb: AlphaColor<Srgb> = color.to_alpha_color();
         srgb.to_rgba8().into()
+    }
+}
+
+impl From<[u8; 3]> for Color {
+    #[inline]
+    fn from(value: [u8; 3]) -> Self {
+        Self::rgb(value[0], value[1], value[2])
+    }
+}
+
+impl From<([u8; 3], f32)> for Color {
+    #[inline]
+    fn from(value: ([u8; 3], f32)) -> Self {
+        let [r, g, b] = value.0;
+        Self::rgba(r, g, b, value.1)
+    }
+}
+
+impl From<&str> for Color {
+    #[inline]
+    fn from(value: &str) -> Self {
+        Self::parse(value)
+    }
+}
+
+impl From<String> for Color {
+    #[inline]
+    fn from(value: String) -> Self {
+        Self::parse(value.as_str())
     }
 }
 
@@ -85,6 +112,6 @@ pub(super) mod helpers {
 
     pub const fn rgba(hex: u32) -> Color {
         let [r, g, b, a] = hex.to_be_bytes();
-        Color::rgba(r, g, b, a as f32 / 255.0)
+        Color { r, g, b, a }
     }
 }
