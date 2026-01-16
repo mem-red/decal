d69784c43ad13bc6b5e8254cd197694a54e8d3cb
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 02:06:00 +0530

    impl `invert` filter preset

---
+++ decal-core/src/filters/presets/invert.rs
@@ -0,0 +1,15 @@
+use crate::filters::Filter;
+use crate::filters::primitives::TransferFunction;
+
+pub fn invert(amount: f32) -> Filter {
+    let a = amount.clamp(0.0, 1.0);
+    let ia = 1.0 - a;
+
+    Filter::new(|ctx| {
+        ctx.component_transfer()
+            .func_r(TransferFunction::table(vec![a, ia]))
+            .func_g(TransferFunction::table(vec![a, ia]))
+            .func_b(TransferFunction::table(vec![a, ia]))
+            .finish();
+    })
+}
--- decal-core/src/filters/presets/mod.rs
+++ decal-core/src/filters/presets/mod.rs
@@ -1,9 +1,11 @@
 mod blur;
 mod grayscale;
 mod hue_rotate;
+mod invert;
 mod sepia;
 
 pub use blur::*;
 pub use grayscale::*;
 pub use hue_rotate::*;
+pub use invert::*;
 pub use sepia::*;
