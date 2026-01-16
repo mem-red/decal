2bcb91676100c36b94b8524d5a8d76c50f8f1823
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 02:09:16 +0530

    impl `hue_rotate` filter preset

--- decal-core/src/filters/presets/hue_rotate.rs
+++ decal-core/src/filters/presets/hue_rotate.rs
@@ -1,5 +1,7 @@
 use crate::filters::Filter;
 
+// https://www.w3.org/TR/filter-effects-1/#huerotateEquivalent
+
 pub fn hue_rotate(angle: f32) -> Filter {
     Filter::new(|ctx| {
         ctx.color_matrix().hue_rotate(angle).finish();
