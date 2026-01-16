e2e9b03eb0303a3a84ac0f52fd36b919562e67da
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 03:01:14 +0530

    add `gradients` example

---
+++ decal-core/examples/gradients/README.md
@@ -0,0 +1,1 @@
+![Gradients](./output.png)
---
+++ decal-core/examples/gradients/src/main.rs
@@ -0,0 +1,29 @@
+use decal::prelude::*;
+use std::f32::consts::FRAC_1_SQRT_2;
+
+fn main() {
+    let mut engine = Engine::new(EngineOptions::default());
+
+    let gradient_list = [
+        LinearGradient::angle(336.0)
+            .stops([(0.0, rgb(0x0000ff)), (FRAC_1_SQRT_2, rgba(0x0000ff00))]),
+        LinearGradient::angle(127.0)
+            .stops([(0.0, rgb(0x00ff00)), (FRAC_1_SQRT_2, rgba(0x00ff0000))]),
+        LinearGradient::angle(217.0)
+            .stops([(0.0, rgb(0xff0000)), (FRAC_1_SQRT_2, rgba(0xff000000))]),
+    ];
+
+    let mut gradients = decal! {
+        Root(640.0, 480.0) {
+            Block {}
+                .size(pct(100.0))
+                .background(gradient_list)
+        }
+    };
+
+    engine
+        .rasterize(&mut gradients, &RasterizeOptions::default())
+        .unwrap()
+        .save_png("./output.png")
+        .unwrap();
+}
