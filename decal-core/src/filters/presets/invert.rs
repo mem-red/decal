ccc8f16b0e46ec2222998b3133279cabd56db356
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 02:09:11 +0530

    impl `invert` filter preset

--- decal-core/src/filters/presets/invert.rs
+++ decal-core/src/filters/presets/invert.rs
@@ -1,6 +1,8 @@
 use crate::filters::Filter;
 use crate::filters::primitives::TransferFunction;
 
+// https://www.w3.org/TR/filter-effects-1/#invertEquivalent
+
 pub fn invert(amount: f32) -> Filter {
     let a = amount.clamp(0.0, 1.0);
     let ia = 1.0 - a;
