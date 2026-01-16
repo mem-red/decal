89a72a034fa97026696289d98b551ec4e76fc0af
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 01:58:00 +0530

    impl `grayscale` filter preset

--- decal-core/src/filters/presets/grayscale.rs
+++ decal-core/src/filters/presets/grayscale.rs
@@ -1,5 +1,35 @@
 use crate::filters::Filter;
 
-pub fn grayscale(value: f32) -> Filter {
-    todo!()
+// https://www.w3.org/TR/filter-effects-1/#grayscaleEquivalent
+
+pub fn grayscale(amount: f32) -> Filter {
+    Filter::new(|ctx| {
+        let x = 1.0 - amount.clamp(0.0, 1.0);
+        ctx.color_matrix()
+            .matrix([
+                [
+                    0.2126 + 0.7874 * x,
+                    0.7152 - 0.7152 * x,
+                    0.0722 - 0.0722 * x,
+                    0.0,
+                    0.0,
+                ],
+                [
+                    0.2126 - 0.2126 * x,
+                    0.7152 + 0.2848 * x,
+                    0.0722 - 0.0722 * x,
+                    0.0,
+                    0.0,
+                ],
+                [
+                    0.2126 - 0.2126 * x,
+                    0.7152 - 0.7152 * x,
+                    0.0722 + 0.9278 * x,
+                    0.0,
+                    0.0,
+                ],
+                [0.0, 0.0, 0.0, 1.0, 0.0],
+            ])
+            .finish();
+    })
 }
