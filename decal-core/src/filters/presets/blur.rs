837482ded44bf722fc2176a8ad26dd4d202e254b
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 02:09:25 +0530

    impl `blur` filter preset

--- decal-core/src/filters/presets/blur.rs
+++ decal-core/src/filters/presets/blur.rs
@@ -1,5 +1,7 @@
 use crate::filters::Filter;
 
+// https://www.w3.org/TR/filter-effects-1/#blurEquivalent
+
 pub fn blur(amount: f32) -> Filter {
     Filter::new(|ctx| {
         ctx.gaussian_blur().std_deviation(amount).finish();
