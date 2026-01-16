00f311a4f03eaf410601f9b03aa0cc6df7048f9a
zignis <77036902+zignis@users.noreply.github.com>
2026-01-17 02:30:40 +0530

    add flood attrs to `DropShadow`

--- decal-core/src/filters/presets/drop_shadow.rs
+++ decal-core/src/filters/presets/drop_shadow.rs
@@ -1,14 +1,15 @@
 use crate::filters::Filter;
-use crate::filters::primitives::TransferFunction;
+use crate::primitives::Color;
 
 // https://www.w3.org/TR/filter-effects-1/#dropshadowEquivalent
 
-pub fn drop_shadow(dx: f32, dy: f32, blur_amount: f32) -> Filter {
+pub fn drop_shadow(dx: f32, dy: f32, blur_amount: f32, color: Option<Color>) -> Filter {
     Filter::new(|ctx| {
         ctx.drop_shadow()
             .dx(dx)
             .dy(dy)
             .std_deviation(blur_amount)
+            .flood_color(color)
             .finish();
     })
 }
