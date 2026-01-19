70ecc1539f44273ca2f30091b5fd4838d42695d7
zignis <77036902+zignis@users.noreply.github.com>
2026-01-19 15:21:23 +0530

    add `text_stencil` example

---
+++ decal-core/examples/text_stencil/src/main.rs
@@ -0,0 +1,47 @@
+use decal::prelude::*;
+
+fn main() {
+    let mut engine = Engine::new(EngineOptions {
+        fonts: FontRegistry::new().load_system_fonts(),
+        ..Default::default()
+    });
+
+    let gradient = LinearGradient::right().stops([
+        (0.3, rgb(0x3f87a6)),
+        (0.6, rgb(0xebf8e1)),
+        (0.9, rgb(0xf69d3c)),
+    ]);
+
+    let mut gradients = decal! {
+        Root(1024.0, 350.0) {
+            Column {
+                Text("type = alpha 🐠")
+                    .stencil(gradient.clone())
+                    .stencil_type(StencilType::Alpha)
+
+                Text("type = luminance 🐠")
+                    .color(rgb(0xffffff))
+                    .stencil(gradient.clone())
+                    .stencil_type(StencilType::Luminance)
+
+                Text("scope = vector glyphs 🐠")
+                    .stencil(gradient)
+                    .stencil_scope(StencilScope::VectorGlyphs)
+            }
+                .size(pct(100.0))
+                .padding(48)
+                .align_items(AlignItems::Stretch)
+                .font_size(64.0)
+                .line_height(80.0)
+                .font_weight(FontWeight::Bold)
+                .text_align(TextAlign::End)
+                .background(rgb(0x000))
+        }
+    };
+
+    engine
+        .rasterize(&mut gradients, &RasterizeOptions::default())
+        .unwrap()
+        .save_png("./output.png")
+        .unwrap();
+}
