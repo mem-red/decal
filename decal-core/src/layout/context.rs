6ad4872f4c1a439453f3de3d02aee39c088d89c8
zignis <77036902+zignis@users.noreply.github.com>
2026-01-24 01:14:07 +0530

    add constructor to `RenderContext`

--- decal-core/src/layout/context.rs
+++ decal-core/src/layout/context.rs
@@ -12,6 +12,7 @@ use std::{
     sync::Arc,
 };
 
+#[derive(Debug)]
 pub(crate) struct RenderContext<'a, T>
 where
     T: Write,
@@ -21,4 +22,24 @@ where
     pub(crate) resources: &'a Mutex<Resources>,
     pub(crate) root_size: Size<f32>,
     pub(crate) options: &'a VectorizeOptions,
+}
+
+impl<'a, T> RenderContext<'a, T>
+where
+    T: Write,
+{
+    #[cfg(test)]
+    pub(crate) fn new(
+        out: &'a mut T,
+        resources: &'a Mutex<Resources>,
+        options: &'a VectorizeOptions,
+    ) -> Self {
+        Self {
+            out,
+            fonts: Default::default(),
+            resources,
+            root_size: Size::from_values(0.0, 0.0),
+            options,
+        }
+    }
 }
