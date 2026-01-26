b50cbf6bf06935ee65b213f88abffc89511cfbf1
zignis <77036902+zignis@users.noreply.github.com>
2026-01-27 04:13:34 +0530

    expose filter primitives properties in prelude

--- decal-core/src/filters/mod.rs
+++ decal-core/src/filters/mod.rs
@@ -4,11 +4,22 @@ mod presets;
 mod primitives;
 mod region;
 //
+
 pub(crate) use context::*;
+
 //
 pub use filter::*;
 pub use presets::*;
 pub use region::*;
+
+// export primitive properties in prelude
+pub use primitives::{
+    ChannelSelector,
+    CompositeOperator,
+    MorphologyOperator,
+    TransferFunction,
+    TurbulenceType,
+};
 
 pub mod filter_primitives {
     pub use super::primitives::*;
