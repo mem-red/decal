6b05ddf6d5ffc2b2b31dc978df404b512b520915
zignis <77036902+zignis@users.noreply.github.com>
2026-02-23 02:36:42 +0530

    prepare `0.6.0` release

--- decal-core/src/lib.rs
+++ decal-core/src/lib.rs
@@ -1,4 +1,13 @@
-#![doc = include_str!("../../README.md")]
+//! # Decal
+//!
+//! `decal` is a declarative rendering library that lets you describe scenes
+//! using a Rust-native DSL and render them to SVG or PNG.
+//!
+//! Scene descriptions are backend-agnostic and can be serialized into vector
+//! (SVG, native) or raster (PNG using [`resvg`]) output while preserving
+//! layout semantics and visual fidelity.
+//!
+//! Checkout the project repository for more details: https://github.com/mem-red/decal
 
 #[forbid(unsafe_code)]
 #[warn(missing_docs)]
